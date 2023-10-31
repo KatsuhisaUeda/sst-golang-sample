50373394e786c5974058f90bf5b3edf8c186d2ca
Katsuhisa Ueda <katuhisa@systems-noel.jp>
2023-10-31 17:51:47 +0900

    Remove functions created by default

--- packages/functions/src/events/todo-created.ts
+++
@@ -1,6 +0,0 @@
-import { Todo } from '@back-sst/core/todo'
-import { EventHandler } from 'sst/node/event-bus'
-
-export const handler = EventHandler(Todo.Events.Created, async (evt) => {
-  console.log('Todo created', evt)
-})
--- packages/functions/src/lambda.ts
+++
@@ -1,8 +0,0 @@
-import { ApiHandler } from 'sst/node/api'
-
-export const handler = ApiHandler(async () => {
-  return {
-    statusCode: 200,
-    body: `Hello world. The time is ${new Date().toISOString()}`,
-  }
-})
