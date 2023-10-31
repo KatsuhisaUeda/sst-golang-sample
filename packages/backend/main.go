c6e24e299034da641485cb22ccffd14ac8155de6
Katsuhisa Ueda <katuhisa@systems-noel.jp>
2023-10-31 17:54:19 +0900

    Add ECS to run docker containers implemented in golang

---
+++ packages/backend/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"rsc.io/quote/v4"
+)
+
+func main() {
+	http.HandleFunc("/", echoHello)
+	http.HandleFunc("/quote", echoQuote)
+	http.HandleFunc("/envs", echoEnviron)
+	http.ListenAndServe(":8000", nil)
+}
+
+func echoHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+func echoQuote(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, quote.Go())
+}
+
+func echoEnviron(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "")
+	for _, e := range os.Environ() {
+		fmt.Fprintln(w, e)
+	}
+}
--- stacks/KataruStack.ts
+++ stacks/KataruStack.ts
@@ -1,1 +1,33 @@
-export function API() {}
+import { Config, Service, StackContext } from 'sst/constructs'
+
+export function API({ stack }: StackContext) {
+  const DBURI = new Config.Parameter(stack, 'DBURI', {
+    value: 'postgres://user:pass@localhost/sampledb1',
+  })
+  const SECRET_KEY = new Config.Secret(stack, 'SECRET_KEY')
+
+  const backend = new Service(stack, 'KataruBackend', {
+    path: './packages/backend',
+    bind: [DBURI, SECRET_KEY],
+    port: 8000,
+    dev: {
+      deploy: true,
+      url: 'http://localhost:8000',
+    },
+    logRetention: 'one_week',
+    cdk: {
+      applicationLoadBalancerTargetGroup: { healthCheck: { path: '/healthz' } },
+      cloudfrontDistribution: false,
+      fargateService: {
+        capacityProviderStrategies: [{ capacityProvider: 'FARGATE', base: 0, weight: 1 }],
+        circuitBreaker: { rollback: true },
+      },
+    },
+  })
+  // // Commented out below due to failure to delete ClusterCapacityProviderAssociations
+  // backend.cdk.cluster.enableFargateCapacityProviders()
+
+  stack.addOutputs({
+    ApiEndpoint: backend.url,
+  })
+}
